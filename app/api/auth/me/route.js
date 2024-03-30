576acf8952b6440694009479501a102f11b1f6ac
Sajid Ali <113129396+coder-169@users.noreply.github.com>
2024-03-30 16:08:25 +0500

    Updated auth

--- app/api/auth/me/route.js
+++ app/api/auth/me/route.js
@@ -7,11 +7,17 @@ import { NextResponse } from "next/server"
 
 export async function GET(req, res) {
     try {
-        await isAuthenticated(req, res)
+        // await isAuthenticated(req, res)
         await dbConnect()
-        const user = await User.findById(req.user).select('-password')
+        const headerList = headers()
+        const token = headerList.get('token')
+        if (!token)
+            return NextResponse.json({ success: false, message: "invalid authorization! please login again" }, { status: 401 })
+        const data = jwt.verify(token, process.env.JWT_SECRET)
+        // req.user = data.id    
+        const user = await User.findById(data.id).select('-password')
         if (!user)
-            return NextResponse.json({ success: false, message: "user not found",   user:req.user }, { status: 404 })
+            return NextResponse.json({ success: false, message: "user not found", data }, { status: 404 })
         console.log(user)
         return NextResponse.json({ success: true, message: "user found successfully", user }, { status: 200 })
     } catch (error) {
